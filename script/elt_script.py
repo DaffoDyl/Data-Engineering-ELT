e3713d041e753b421257265e72ac5e3c9a3bec56
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-08-20 01:04:17 -0600

    Finish script

--- script/elt_script.py
+++ script/elt_script.py
@@ -46,3 +46,35 @@ destination_config = {
     'host': 'destination_postgres'
 }
 
+# Use pg_dump to dump the source database to a SQL file
+dump_command = [
+    'pg_dump',
+    '-h', source_config['host'],
+    '-U', source_config['user'],
+    '-d', source_config['dbname'],
+    '-f', 'data_dump.sql',
+    '-w'  # Do not prompt for password
+]
+
+# Set the PGPASSWORD environment variable to avoid password prompt
+subprocess_env = dict(PGPASSWORD=source_config['password'])
+
+# Execute the dump command
+subprocess.run(dump_command, env=subprocess_env, check=True)
+
+# Use psql to load the dumped SQL file into the destination database
+load_command = [
+    'psql',
+    '-h', destination_config['host'],
+    '-U', destination_config['user'],
+    '-d', destination_config['dbname'],
+    '-a', '-f', 'data_dump.sql'
+]
+
+# Set the PGPASSWORD environment variable for the destination database
+subprocess_env = dict(PGPASSWORD=destination_config['password'])
+
+# Execute the load command
+subprocess.run(load_command, env=subprocess_env, check=True)
+
+print("Ending ELT script...")
